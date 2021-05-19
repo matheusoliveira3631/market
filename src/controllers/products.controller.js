a357c48495d795e8d20c5b53084dd92d05bbe249
Massacote Reborn <51460309+matheusoliveira3631@users.noreply.github.com>
2021-05-19 02:33:12 -0300

    fix products put/delete routes, fiz bug where successfull post would return 500

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -3,55 +3,39 @@ const {deleteProduct, updateProduct} = require('../database/product/editProduct'
 const {findProductBy, serializeProducts} = require('../database/product/findProduct')
 
 module.exports = class productController {
-    serialize(req, res){
+    async serialize(req, res){
+        serializeProducts().then((productList)=>{
+            res.status(200).json(productList)
+        }).catch((err)=>{
+            res.status(500).json({message:err})
+        })
+        
+    }
+
+    async createProduct(req, res){
         try{
-            serializeProducts().then((productList)=>{
-                res.status(200).json(productList)
-            })
+        await createProduct(req.body)
+        res.status(201).json({message:'ok'})
         }catch(err){
             res.status(500).json({message:err})
         }
     }
 
-    createProduct(req, res){
+    async updateProduct(req, res){
         try{
-            createProduct(req.body).then((message)=>{
-                if (message!='ok'){
-                    res.status(500).json({message:message})
-                }else{
-                    res.status(201).json({message:'ok'})
-                }
-            })
+        await updateProduct('barcode', {old:req.params.id, new:req.body})
+        res.status(200).json({message:'ok'})
         }catch(err){
             res.status(500).json({message:err})
         }
     }
 
-    updateProduct(req, res){
+    async deleteProduct(req, res){
         try{
-            updateProduct('barcode', {old:req.params.id, new:req.body}).then((message)=>{
-                if (message!='ok'){
-                    res.status(500).json({message:message})
-                }else{
-                    res.status(200).json({message:'ok'})
-                }
-            })
+        await deleteProduct('barcode', req.params.id)
+        res.status(200).json({message:'ok'})
         }catch(err){
             res.status(500).json({message:err})
         }
-    }
-
-    deleteProduct(req, res){
-        try{
-            deleteProduct('barcode', req.params.id).then((message)=>{
-                if (message!='ok'){
-                    res.status(500).json({message:message})
-                }else{
-                    res.status(200).json({message:'ok'})
-                }
-            })
-        }catch(err){
-            res.status(500).json({message:message})
-        }
-    }
+}
 }
--- src/database/product/editProduct.js
+++ src/database/product/editProduct.js
@@ -4,21 +4,32 @@ const deleteProduct = async function(param, value){
     const query={}
     query[param]=value
     try{
-        product.deleteOne(query)
-        return 'ok'
+        product.deleteOne(query, (err)=>{
+            if (err){
+                throw err
+            }else{
+                return 'ok'
+            }
+        })
     }catch(err){
-        return err
+        throw err
     }
 }
 
 const updateProduct = async function(param, values){
     const query={}
     query[param]=values['old']
+    console.log(values['new'])
     try{
-        product.updateOne(query, values['new'])
-        return 'ok'
+        product.updateOne(query, values['new'], (err)=>{
+            if (err){
+                throw err
+            }else{
+                return 'ok'
+            }
+        })
     }catch(err){
-        return err
+        throw err
     }   
 }
 
